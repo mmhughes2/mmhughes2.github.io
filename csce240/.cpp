3cc15161e58fbf270a3cf61a3e989065102bc65e
mmhughes2 <kenhughes925@gmail.com>
2025-01-28 15:53:12 -0500

    Assignment 4 - 1st attempt

---
+++ csce240/.cpp
@@ -0,0 +1,15 @@
+// Copyright Mackenzie Hughes
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main () {
+    int height, feet, inches; 
+    cout << "Enter your height in inches ";
+    cin >> height;
+    feet = height/12;
+    inches = height % 12;
+    cout << height << " inches" = ""
+        
+}
