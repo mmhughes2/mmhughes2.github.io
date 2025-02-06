f48fb4cd8583263416c4ef49bebaad0a00056a2e
mmhughes2 <kenhughes925@gmail.com>
2025-02-06 08:28:37 -0500

    Javascript Practice

---
+++ csce242/exercises/06js_intro/script.js
@@ -0,0 +1,13 @@
+//do stuff when button clicked
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("WOW!");
+    document.getElementById("result").innerHTML="Hi Portia";
+};
+
+//change color
+document.getElementById("btn-color").onclick = () => {
+    const messageP = document.getElementById("message");
+    messageP.innerHTML = "Good bye";
+    messageP.classList.toggle("sad");
+
+}
