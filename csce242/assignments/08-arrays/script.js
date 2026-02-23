9bfb122fee3b456033363b16bda8a5b7d728fb5c
mmhughes2 <kenhughes925@gmail.com>
2026-02-22 21:38:45 -0500

    making formatting changes to assignment 8

--- csce242/assignments/08-arrays/script.js
+++ csce242/assignments/08-arrays/script.js
@@ -3,37 +3,37 @@
 // Happy songs
 const happySongs = {
     "Happy by Pharrell Williams":
-        "https://www.youtube.com/embed/ZbZSe6N_BXs",
+        "https://www.youtube.com/embed/ZbZSe6N_BXs?si=4YAq7UJSaK5iI_sD",
 
-    "Don't Stop Me Now by Queen":
-        "https://www.youtube.com/embed/HgzGwKwLmgM",
+    "Walking on Sunshine by Katrina & the Waves":
+        "https://www.youtube.com/embed/iPUmE-tne5U?si=fGMJZdzGOq9qFcTV",
 
     "Can't Stop the Feeling by Justin Timberlake":
-        "https://www.youtube.com/embed/ru0K8uYEZWw",
+        "https://www.youtube.com/embed/ru0K8uYEZWw?si=IQ_PgNex63Z4nuL1",
 
     "Don't Worry Be Happy by Bobby McFerrin":
-        "https://www.youtube.com/embed/d-diB65scQU",
+        "https://www.youtube.com/embed/d-diB65scQU?si=10Y4t2IIqbws6wcS",
 
-    "I'm Walking on Sunshine by Katrina & the Waves":
-        "https://www.youtube.com/embed/iPUmE-tne5U"
+    "Party in the U.S.A. by Miley Cyrus":
+        "https://www.youtube.com/embed/M11SvDtPBhA?si=A2tDGxGOkARY16z4"
 };
 
 // Sad songs
 const sadSongs = {
     "Happier Than Ever by Billie Eilish":
-        "https://www.youtube.com/embed/5GJWxDKyk3A",
+        "https://www.youtube.com/embed/5GJWxDKyk3A?si=RDvAtIAENhliXLel",
 
     "Someone You Loved by Lewis Capaldi":
-        "https://www.youtube.com/embed/zABLecsR5UE",
+        "https://www.youtube.com/embed/zABLecsR5UE?si=L4JYXSZ546U5HCJH",
 
     "Someone Like You by Adele":
-        "https://www.youtube.com/embed/hLQl3WQQoQ0",
+        "https://www.youtube.com/embed/hLQl3WQQoQ0?si=OcsCuXA0UNeq6PKp",
 
-    "Fix You by Coldplay":
-        "https://www.youtube.com/embed/k4V3Mo61fJM",
+    "The Night We Met by Lord Huron":
+        "https://www.youtube.com/embed/KtlgYxa6BMU?si=_PplkAnuQbxXqmLD",
 
-    "Hurt by Johnny Cash":
-        "https://www.youtube.com/embed/8AHCfZTRGiI"
+    "Session 32 by Summer Walker":
+        "https://www.youtube.com/embed/tyxJYWvQIbY?si=kAFCCLqFfQOCwhpe"
 };
 
 const moodSelect = document.getElementById("mood");
